d371954b20712b75e5cb2265b9e5f0ec454a60f0
Frank Lee <flee531@gmail.com>
2016-06-20 14:36:55 -0700

    Initial fooling around with the sprint

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,47 @@
 // YOUR CODE HERE:
+var messages = [];
+var prevMessages = [];
+
+// Queries the PARSE server to obtain the 100 most recent messages
+var getMessages = function() {
+    $.get('https://api.parse.com/1/classes/messages', function(msg) { 
+        prevMessages === messages;
+        messages = msg.results;    // Results is the array of stored messages
+    });
+};
+
+// Adds new messages as elements on the DOM
+var displayMessages = function(messages) {
+    // Filters the recently acquired messages to identify new messages
+    // Uses the 'updatedAt' timestamp to identify new messages
+    var newMessages;
+    var mostRecentMessageTime;
+    
+    if (prevMessages.length > 0) {
+        mostRecentMessageTime = Date.parse(prevMessages[0].updatedAt);
+
+        newMessages = _.filter(messages, function(msg) {
+            if (Date.parse(msg.updatedAt) > mostRecentMessageTime) {
+                return msg;
+            }
+        });    
+    } else {
+        newMessages = messages;
+    }
+   
+   // Append each new message to the DOM
+   _.each(newMessages, function(msg) {
+    $('#chats').append('<div><p>' + msg.text + '</p></div>');
+    });
+    
+    console.log(newMessages);
+};
+
+// Main message retrieval/display loop
+getMessages();
+
+setInterval(function() {
+    getMessages();
+    displayMessages(messages);
+}, 3000);
 
