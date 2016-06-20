c5ff028d5a55868f28f4faf256cdf0bbd6ec438d
mredmundto <yatlunto@gmail.com>
2016-06-20 15:03:38 -0700

    Implement getMessages and displayMessages functions

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,47 +1,24 @@
-// YOUR CODE HERE:
+// Stores all messages retrieved from the server
+// The messages are in reverse chronological order,
+// so message[0] is the most recent message retrieved
+// from the PARSE server
 var messages = [];
-var prevMessages = [];
 
-// Queries the PARSE server to obtain the 100 most recent messages
+// Retrieves the most recent 100 messages from the server
 var getMessages = function() {
-    $.get('https://api.parse.com/1/classes/messages', function(msg) { 
-        prevMessages === messages;
-        messages = msg.results;    // Results is the array of stored messages
-    });
-};
-
-// Adds new messages as elements on the DOM
-var displayMessages = function(messages) {
-    // Filters the recently acquired messages to identify new messages
-    // Uses the 'updatedAt' timestamp to identify new messages
-    var newMessages;
-    var mostRecentMessageTime;
-    
-    if (prevMessages.length > 0) {
-        mostRecentMessageTime = Date.parse(prevMessages[0].updatedAt);
-
-        newMessages = _.filter(messages, function(msg) {
-            if (Date.parse(msg.updatedAt) > mostRecentMessageTime) {
-                return msg;
-            }
-        });    
-    } else {
-        newMessages = messages;
-    }
-   
-   // Append each new message to the DOM
-   _.each(newMessages, function(msg) {
-    $('#chats').append('<div><p>' + msg.text + '</p></div>');
-    });
-    
-    console.log(newMessages);
-};
-
-// Main message retrieval/display loop
-getMessages();
-
-setInterval(function() {
-    getMessages();
+  $.get('https://api.parse.com/1/classes/messages', function(msg) {
+    messages = msg.results;
     displayMessages(messages);
-}, 3000);
+  });
 
+};
+
+// Appends new messages as elements to the DOM
+var displayMessages = function(messages) {
+  _.each(messages, function(msg) {
+    var ele = '<div><p>' + msg.text + '</div></p>';
+    $('#chats').prepend(ele);
+  });
+};
+
+getMessages();
